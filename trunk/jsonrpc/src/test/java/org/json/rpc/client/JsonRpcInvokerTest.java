9721d1f25a1b6f99b7b2ac47ea471063865ec75c
nix@develnix.com <nix@develnix.com@ac86d361-68fc-c0e7-97bd-cde08e9112d2>
2011-02-17 17:42:19 +0000

    updated license/readme

--- trunk/jsonrpc/src/test/java/org/json/rpc/client/JsonRpcInvokerTest.java
+++ trunk/jsonrpc/src/test/java/org/json/rpc/client/JsonRpcInvokerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 devel.nix@gmail.com
+ * Copyright (C) 2011 Develnix.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
